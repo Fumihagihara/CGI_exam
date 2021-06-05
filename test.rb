fbea51da618a7d5b0415c1e52290f36828d7a9ff
fumi hagihara <7676.fmhghr@gmail.com>
2021-06-05 19:07:54 +0900

    CGIアプリケーション課題2

--- test.rb
+++ test.rb
@@ -7,8 +7,8 @@ server = WEBrick::HTTPServer.new({
 ['INT', 'TERM'].each {|signal|
   Signal.trap(signal){ server.shutdown }
 }
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'kadai.html.erb')
 server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
-server.mount('/kadai', WEBrick::HTTPServlet::ERBHandler, 'kadai.html.erb')
 server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
 server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
 server.mount('/give_for.cgi', WEBrick::HTTPServlet::CGIHandler, 'give_for.rb')
